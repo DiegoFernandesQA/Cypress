376557b6956e61ddbb4a8483eed3345836750b28
Diego Fernandes <diego.fernandes@enjoei.com.br>
2020-12-16 22:48:31 -0300

    learning cypress

---
+++ cypress/integration/teste.spec.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+const { describe } = require("mocha")
+
+cy.check()
+
+describe('', () => {
+    it('', () => {
+        
+    }  
+    it('', () => {
+        
+    }
+}
